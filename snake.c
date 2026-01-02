e1673cd304897079801291c498a258ed89a08666
Tom Damien <tom.damien@student-cs.fr>
2026-01-02 14:58:38 +0100

    fps counter

--- snake.c
+++ snake.c
@@ -18,6 +18,13 @@ typedef struct{
     int case_y;
 }Pomme;
 
+typedef struct{
+    int case_x;
+    int case_y;
+    char dir;
+    struct snake* next;
+}snake;
+
 Pomme pommes[Nb_Pommes];
 
 void draw_circle(SDL_Renderer* r, int cx, int cy, int rad){
@@ -53,9 +60,15 @@ int main(){
     pommes[4].case_x=7;
     pommes[4].case_y=5;
 
+    Uint64 freq = SDL_GetPerformanceFrequency();               // fréquence du compteur de performance pour la gestion du framerate, c'est à dire le nombre de ticks par seconde
+    Uint64 debut = SDL_GetPerformanceCounter();                // compteur de performance au début de la boucle, c'est à dire le nombre de ticks depuis le démarrage de SDL != 0
+
 
     int running=1;
     while(running){
+        Uint64 fin = SDL_GetPerformanceCounter();                   // compteur de performance à la fin de la boucle
+        Uint64 elapsed_ticks = fin - debut;                      // nombre de ticks écoulés depuis le début de la boucle                                            
+        Uint64 target_ticks = (Uint64)((double)freq / 60);
         SDL_Event e;
         while (SDL_PollEvent(&e)){              
             if (e.type == SDL_QUIT) running = 0;}
@@ -63,12 +76,19 @@ int main(){
         SDL_RenderClear(r);
 
         SDL_SetRenderDrawColor(r,180,30,30,255);
+        /* dessine les pommes*/
         for(int i=0; i<Nb_Pommes; i++){
             Pomme P=pommes[i];
             draw_circle(r, P.case_x*taille_case+taille_case/2, P.case_y*taille_case+taille_case/2, rayon_pomme);
         }
+        double delta = (double)elapsed_ticks/(double)(freq);       // temps écoulé en secondes
+        double fps = 1.0/delta; // calcul du FPS
+        printf("%f\n", fps);
+
 
         SDL_RenderPresent(r);
+
+        debut = fin;
     }
 
     SDL_DestroyWindow(w);
