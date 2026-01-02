518ccd2198aaf62b69e26757f4ef0bf2fce0af2d
Tom Damien <tom.damien@student-cs.fr>
2026-01-02 02:07:03 +0100

    5 apples

--- snake.c
+++ snake.c
@@ -4,8 +4,22 @@
 #include <math.h>
 
 
-#define width 1080
-#define height 720
+#define width 900
+#define height 900
+#define Nb_case 9
+#define taille_case 100
+#define rayon_pomme 45
+#define Nb_Pommes 5
+
+
+typedef struct{
+    int id;
+    int case_x;
+    int case_y;
+}Pomme;
+
+Pomme pommes[Nb_Pommes];
+
 void draw_circle(SDL_Renderer* r, int cx, int cy, int rad){
     for (int dy =-rad; dy<=rad;dy++){
         int dx = (int)sqrt(rad*rad-dy*dy);
@@ -13,15 +27,32 @@ void draw_circle(SDL_Renderer* r, int cx, int cy, int rad){
     }
 }
 
+
+
 int main(){
     SDL_Window* w = NULL;
-    w = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
-                                                                SDL_WINDOWPOS_UNDEFINED,
-                                                                width,
-                                                                height,
-                                                                SDL_WINDOW_SHOWN);
+    w = SDL_CreateWindow("Snake",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,width,height,SDL_WINDOW_SHOWN);
     SDL_Renderer* r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED); 
     SDL_SetRenderDrawBlendMode(r, SDL_BLENDMODE_BLEND);
+
+
+    /* Initiailisation des pommes*/
+    pommes[0].id=0;
+    pommes[0].case_x=5;
+    pommes[0].case_y=3;
+    pommes[1].id=1;
+    pommes[1].case_x=7;
+    pommes[1].case_y=3;
+    pommes[2].id=2;
+    pommes[2].case_x=6;
+    pommes[2].case_y=4;
+    pommes[3].id=3;
+    pommes[3].case_x=5;
+    pommes[3].case_y=5;
+    pommes[4].id=4;
+    pommes[4].case_x=7;
+    pommes[4].case_y=5;
+
 
     int running=1;
     while(running){
@@ -32,7 +63,10 @@ int main(){
         SDL_RenderClear(r);
 
         SDL_SetRenderDrawColor(r,180,30,30,255);
-        draw_circle(r, width/2, height/2, 10);
+        for(int i=0; i<Nb_Pommes; i++){
+            Pomme P=pommes[i];
+            draw_circle(r, P.case_x*taille_case+taille_case/2, P.case_y*taille_case+taille_case/2, rayon_pomme);
+        }
 
         SDL_RenderPresent(r);
     }
